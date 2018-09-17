ba83a0432727212a0d539c99e14095ec5f48921f
jiidapa <jidapa.p@kkumail.com>
2018-09-17 22:05:03 +0700

    resource

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Easy Job</title>
         <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}"/>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-grid.css')}}"/>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-reboot.css')}}"/>
+
+        <link href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500|Quicksand:300,400,500" rel="stylesheet">
+        
         <script src="{{asset('js/angularjs.min.js')}}"></script>
         <script src="{{asset('js/jquery.js')}}"></script>
         <script type="text/javascript">
@@ -19,10 +25,17 @@
     <body>
         @yield('body')
 
+        <header>
+        
+        </header>
+
+        
         <footer>
         
         </footer>
         <script src="{{asset('js/MainController/MainJs.js')}}"></script>
+        <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+        <script src="{{asset('js/bootstrap.js')}}"></script>
         @yield('script')  
     </body>
 </html>
