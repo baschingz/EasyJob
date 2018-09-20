168cc51f4590ad84e88be39f8ea5e441fb4fad12
Atisit Thongbai <bom_144440@outlook.co.th>
2018-09-20 16:25:12 +0700

    'add_seeder'

--- resources/views/Register/RegisterForm.blade.php
+++ resources/views/Register/RegisterForm.blade.php
@@ -1,7 +1,6 @@
 @extends('main')
 
 @section('body')
-<<<<<<< HEAD
 <style>
   div.inputreg{
     padding: 60px;
@@ -18,76 +17,9 @@
 <div class="col-sm-7 ml-auto">
   <button id='em' class="btn btn-info btn-lg">EMPLOYEE</button>
   <button id='job' class="btn btn-primary btn-lg">JOBSEEKER</button>
-=======
-<div>
-    <button id='em'>
-        em
-    </button>
-    <button id='job'>
-        job
-    </button>
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
 </div>
 <hr class="my-4">
 <div ng-controller='RegisterController'>
-<<<<<<< HEAD
-  <div id='employerForm' >
-    <!-- registere form for employer -->
-    <div class="row">
-      <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2 col-lg-2"></div>
-      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-        <br>
-        <h2 style="background-color: pink; margin-bottom: 0px; padding: 6px; padding-left: 20px;">Employee</h2>
-        <div class="inputreg">
-          <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" placeholder="ชื่อ">
-            </div>
-          </div>
-          <br>
-          <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" id="nameShop" placeholder="ชื่อร้าน">
-            </div>
-          </div>
-          <br>
-          <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" id="address" placeholder="ที่อยู่ร้าน">
-            </div>
-          </div>
-          <br>
-          <div class="form-row">
-            <div class="col-4">
-              <select class="form-control">
-                <option>ร้านนม</option>
-                <option>ร้านข้าว</option>
-                <option>ร้านขนม</option>
-              </select>
-            </div>
-            <div class="col">
-              <input type="file" class="form-control-file" id="exampleFormControlFile1">
-            </div>
-          </div>
-          <br>
-          <div class="form-row">
-            <div class="col-4">
-              <input type="text" class="form-control" id="phonenumber" placeholder="เบอร์โทรร้าน">
-            </div>
-            <div class="col">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                placeholder="Enter email">
-            </div>
-
-          </div>
-          <br>
-          <div class="form-row">
-            <div class="col-sm-7 ml-auto">
-              <input type="submit" class="btn btn-success" id="submit" value="SUBMIT">
-            </div>
-          </div>
-          <br>
-=======
     <div id='employerForm'>
         <!-- registere form for employer -->
 
@@ -151,25 +83,10 @@
 
                 </div>
             </div>
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
         </div>
     </div>
   </div>
 
-<<<<<<< HEAD
-    <div id='jobseekerForm'  style='display:none;' >
-     <!-- registere form for jobseeker -->
-     <div class="row">
-        <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2 col-lg-2"></div>
-        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-          <br>
-          <h2 style="background-color: pink; margin-bottom: 0px; padding: 6px; padding-left: 20px;" onloadedmetadata="" onloadedmetadata=""onloadedmetadata="">Jobseeker</h2>
-          <div class="inputreg">
-          <div class="form-row">
-            <div class="col-8">
-              <input type="text" class="form-control" placeholder="ชื่อ">
-            </div>
-=======
     <div id='jobseekerForm' style='display:none;'>
         <!-- registere form for jobseeker -->
         <div class="row">
@@ -204,31 +121,12 @@
 
                 <label for="exampleFormControlTextarea1"> รูปถ่าย </label>
                 <input type="file" class="form-control-file" id="exampleFormControlFile1">
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
 
             </div>
-<<<<<<< HEAD
-          </div>
-
-          <label for="exampleFormControlTextarea1"> รูปถ่าย </label>
-          <input type="file" class="form-control-file" id="exampleFormControlFile1">
-          <div class="form-row">
-            <div class="col-sm-7 ml-auto">
-              <input type="submit" class="btn btn-success" id="submit" value="SUBMIT">
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
-</div>
-
-=======
         </div>
     </div>
 </div>
 </div>
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
 @endsection
 
 @section('script')
@@ -251,8 +149,4 @@
         });
     });
 </script>
-<<<<<<< HEAD
 @endsection
-=======
-@endsection
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,32 +1,5 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}" ng-app="myApp">
-<<<<<<< HEAD
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Easy Job</title>
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}"/>
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-grid.css')}}"/>
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-reboot.css')}}"/>
-
-        <link href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500|Quicksand:300,400,500" rel="stylesheet">
-
-        <script src="{{asset('js/angularjs.min.js')}}"></script>
-        <script src="{{asset('js/jquery.js')}}"></script>
-        <script type="text/javascript">
-            var base_path = "{{url('/')}}";
-        </script>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        @yield('import')
-    </head>
-    <body>
-        @yield('body')
-=======
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
 
 <head>
     <meta charset="utf-8">
@@ -57,13 +30,9 @@
 
     <section class="section-navbar">
         <header>
-<<<<<<< HEAD
-
-=======
             <nav class="navbar navbar-light bg-light text-nav">
                 <!-- <a class="navbar-brand" href="#">Easy Job</a> -->
             </nav>
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
         </header>
     </section>
 
@@ -88,17 +57,4 @@
     @yield('script')
 </body>
 
-<<<<<<< HEAD
-
-        <footer>
-
-        </footer>
-        <script src="{{asset('js/MainController/MainJs.js')}}"></script>
-        <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
-        <script src="{{asset('js/bootstrap.js')}}"></script>
-        @yield('script')
-    </body>
 </html>
-=======
-</html>
->>>>>>> c897950630f908001aee1675965f2f36ef5ad30b
